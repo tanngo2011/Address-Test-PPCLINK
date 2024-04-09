e134705fa67d57bec999d52ecd300ec3f2a11ff8
tanngo2011 <manhtanngo2011@gmail.com>
2024-04-09 23:24:03 +0700

    [Update]

--- src/main/java/address/Backend/BackendApplication.java
+++ src/main/java/address/Backend/BackendApplication.java
@@ -1,13 +1,33 @@
 package address.Backend;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BackendApplication {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class BackendApplication implements CommandLineRunner {
+
+	public static void main(String[] args) throws IOException, CsvValidationException {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+//		String[] nextLine;
+//		try (CSVReader reader = new CSVReader(new FileReader("district.csv"))) {
+//			while ((nextLine = reader.readNext()) != null) {
+//				// Truy cập đến từng giá trị trong dòng hiện tại
+//				for (String value : nextLine) {
+//					System.out.println(value);
+//				}
+//			}
+//		}
+	}
 }
